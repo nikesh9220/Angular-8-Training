d20e0775e3e445efdafa862e497f08089cf3a037
Nikesh Pandya <33998764+nikesh9220@users.noreply.github.com>
2019-10-24 18:03:03 +0530

    Angular Training

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SimpleComponent } from './components/simplecomponent/app.simple.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CalculateComponent } from './components/calculatecomponent/app.calculate.component';
+import { Student } from './models/entities/app.student.entity';
+import { StudentComponent } from './components/studentcomponent/app.student.component';
+import { CourseParentComponent } from './components/masterdetails/app.courseparent.component';
+import { StudentChildComponent } from './components/masterdetails/app.studentchild.component';
+import { UtilityServiceComponent } from './components/utilityservicecomponent/app.utilityservice.component';
+import { CourseSenderComponent } from './components/masterdetailsservices/app.coursesender.component';
+import { StudentReceiverComponent } from './components/masterdetailsservices/app.studentreceiver.component';
+import { StudentSearchComponent } from './components/searchwithservices/app.studentsearch.component';
+import { CourseSearchComponent } from './components/searchwithservices/app.coursesearch.component';
+import { SearchComponent } from './components/searchwithservices/app.search.component';
+import { SharedServiceComponent } from './components/sharedservicecomponent/app.sharedservice.component';
+import { SharedModule } from 'src/sharedmodule/app.shared.module';
+import { SharedSearchComponent } from './components/sharedservicecomponent/app.sharedsearch.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleComponent,
+    CalculateComponent,
+    StudentComponent,
+    CourseParentComponent,
+    StudentChildComponent,
+    UtilityServiceComponent,
+    CourseSenderComponent,
+    StudentReceiverComponent,
+    SearchComponent,
+    CourseSearchComponent,
+    StudentSearchComponent,
+    SharedServiceComponent,
+    SharedSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [SharedServiceComponent,SharedSearchComponent],
+ // bootstrap: [SearchComponent,CourseSearchComponent,StudentSearchComponent]
+})
+export class AppModule { }
